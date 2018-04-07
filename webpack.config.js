1c375453ea4e69b5add290f6d8a55f98a397656d
Carlos Perez <solracias@gmail.com>
2018-04-07 12:31:43 -0700

    webpack complete

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+import path from 'path';
+
+module.exports = {
+  // the entry file for the bundle
+  entry: path.join(__dirname, 'client/src/app.jsx'),
+
+  // the bundle file we weill get in the result
+  output: {
+    path: path.join(__dirname, 'client/dist/js'),
+    filename: 'app.js',
+  },
+
+  module: {
+    // apply loaders to files that meet given conditions
+    loaders: [{
+      test: /\.jsx?$/,
+      include: path.join(__dirname, '/client/src'),
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015']
+      }
+    }],
+  },
+
+  // start webpack in a watch mode, so webpack
+  // will rebuild the bundle on changes
+  watch: true
+}
