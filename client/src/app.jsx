f31f7f3c2156b99453584809e4ff4ad848a97756
Carlos Perez <solracias@gmail.com>
2018-04-08 10:48:04 -0700

    app.jsx entry file connected to routes now. Now I need to troubleshoot to get all the components and routes working in order

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import HomePage from './components/HomePage.jsx';
+// import HomePage from './components/HomePage.jsx';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import { browserHistory, Router } from 'react-router';
+import routes from './routes.js';
 
+// v1
 // ReactDom.render(
 //   React.createElement('h1', null, 'Hello from React'),
 //   document.getElementById('react-app')
 // );
 
-ReactDom.render(<HomePage />, document.getElementById('react-app'));
+// v2
+// ReactDom.render(<HomePage />, document.getElementById('react-app'));
+
+// v3
+injectTapEventPlugin();
+
+ReactDom.render((
+  <MuiThemeProvider muiTheme={getMuiTheme()}>
+    <Router history={browserHistory} routes={routes} />
+  </MuiThemeProvider>
+),
+  document.getElementById('react-app')
+);
