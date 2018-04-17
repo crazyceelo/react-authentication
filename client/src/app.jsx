ae21ac145bdef30d4f249e7555983e72f49298c3
Carlos Perez <solracias@gmail.com>
2018-04-16 22:05:38 -0700

    test

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -5,23 +5,11 @@ import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { browserHistory, Router } from 'react-router';
 import routes from './routes.js';
-import HomePage from './components/HomePage.jsx';
 
-
-// v1
-// ReactDom.render(
-//   React.createElement('h1', null, 'Hello from React'),
-//   document.getElementById('react-app')
-// );
-
-// v2
-ReactDom.render(<HomePage />, document.getElementById('react-app'));
-
-// v3
 // remove tap delay, essential for MaterialUI to work properly
-// injectTapEventPlugin();
+injectTapEventPlugin();
 
-// ReactDom.render((
-//   <MuiThemeProvider muiTheme={getMuiTheme()}>
-//     <Router history={browserHistory} routes={routes} />
-//   </MuiThemeProvider>), document.getElementById('react-app'));
+ReactDom.render((
+  <MuiThemeProvider muiTheme={getMuiTheme()}>
+    <Router history={browserHistory} routes={routes} />
+  </MuiThemeProvider>), document.getElementById('react-app'));
